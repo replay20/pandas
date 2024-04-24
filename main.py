55002ca14fe1e6e197c8b24170ea5cdb62f7de88
WPawlo <147150212+WPawlo@users.noreply.github.com>
2024-04-24 12:30:41 +0200

    zad 2 bez ostatniego podpunktu gotowe

--- main.py
+++ main.py
@@ -18,3 +18,42 @@ print(df[df['Liczba']>1000])
 
 print(df[df['Imie']=='DENIS'])
 
+#sumę wszystkich urodzonych dzieci w całym danym okresie,
+
+suma = df['Liczba'].sum()
+print(suma)
+
+#sumę dzieci urodzonych w latach 2000-2005
+
+przedzial = df[(df['Rok']<=2005) & (df['Rok']>=2000)]
+suma2 = przedzial['Liczba'].sum()
+print(suma2)
+
+#sumę urodzonych chłopców i dziewczynek,
+
+chlopcy = df[df['Plec']=='M']
+sum_chlopcy = chlopcy['Liczba'].sum()
+print(sum_chlopcy)
+
+dziewczynki = df[df['Plec']=='K']
+sum_dziewczynki = dziewczynki['Liczba'].sum()
+print(sum_dziewczynki)
+
+#najbardziej popularne imię dziewczynki i chłopca w danym roku ( czyli po 2 rekordy na rok),
+
+for rok in range(2000,2018,1):
+    dziewczynkirok = dziewczynki[dziewczynki['Rok'] == rok]
+    sortd=dziewczynkirok.sort_values(by='Liczba', ascending=False)
+    popd=sortd.iloc[0]
+    print(popd)
+
+    chlopcyrok = chlopcy[chlopcy['Rok'] == rok]
+    sortc = chlopcyrok.sort_values(by='Liczba', ascending=False)
+    popc = sortc.iloc[0]
+    print(popc)
+
+#najbardziej popularne imię dziewczynki i chłopca w całym danym okresie,
+
+sortd=dziewczynki.sort_values(by='Liczba', ascending=False)
+popd=sortd.iloc[0]
+print(popd)
